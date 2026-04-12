8a54bc6c8d38051bdb9e92ce5c84856ab1eaf272
Andresd720 <andresd720@gmail.com>
2026-04-12 09:34:31 +0200

    Add MS-dashboard

---
+++ ms-dashboard/src/main/java/com/bytecodes/MsDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.bytecodes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsDashboardApplication.class, args);
+	}
+
+}
