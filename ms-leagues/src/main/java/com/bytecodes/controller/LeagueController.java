7c5e51105b8472ef88ead44860923b8b2132dd47
Andresd720 <andresd720@gmail.com>
2026-04-16 22:20:19 +0200

    create controller

---
+++ ms-leagues/src/main/java/com/bytecodes/controller/LeagueController.java
@@ -0,0 +1,34 @@
+package com.bytecodes.controller;
+
+
+import com.bytecodes.dto.response.LeagueDetailResponse;
+import com.bytecodes.dto.response.LeagueResponse;
+import com.bytecodes.service.LeagueService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/leagues")
+public class LeagueController {
+
+    private final LeagueService leagueService;
+
+    public LeagueController(LeagueService leagueService) {
+        this.leagueService = leagueService;
+    }
+
+    @GetMapping
+    public List<LeagueResponse> getLeagues(
+            @RequestParam(required = false) String country,
+            @RequestParam(required = false) Integer season,
+            @RequestParam(required = false) Integer id
+    ) {
+        return leagueService.getLeagues(country, season, id);
+    }
+
+    @GetMapping("/{leagueId}")
+    public LeagueDetailResponse getLeagueById(@PathVariable("leagueId") int id) {
+        return leagueService.getLeagueById(id);
+    }
+}
