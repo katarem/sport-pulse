b84bd99bacd01381b7d1eadc43b0e80f8e37ec8e
Andresd720 <andresd720@gmail.com>
2026-04-21 22:14:47 +0200

    Add handlers required

--- ms-teams/src/main/java/com/bytecodes/exception/GlobalHandler.java
+++ ms-teams/src/main/java/com/bytecodes/exception/GlobalHandler.java
@@ -2,10 +2,18 @@ package com.bytecodes.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
+import java.util.Objects;
+
+@RestControllerAdvice
 public class GlobalHandler {
 
+
+// Exception requerida en el proyecto.
     @ExceptionHandler(TeamNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleLeagueNotFoundException(TeamNotFoundException e) {
         return  ResponseEntity
@@ -13,5 +21,22 @@ public class GlobalHandler {
                 .body(ErrorResponse.of("TEAM_NOT_FOUND",
                         e.getMessage())
                 );
+    }
+// Exception del @SpringQueryMap
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public  ResponseEntity<ErrorResponse> handleInvalidParam(MethodArgumentNotValidException e) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ErrorResponse.of("INVALID_REQUEST", Objects.requireNonNull(e.getBindingResult()
+                                .getFieldError())
+                        .getDefaultMessage()));
+    }
+// Excepcion si el parametro esperado es un Int y se para un String
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ErrorResponse> handleInvalidParam(MethodArgumentTypeMismatchException e) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ErrorResponse.of("INVALID_REQUEST",
+                        "El Parametro introducido no es un numero, validelo e intente de nuevo"));
     }
 }
