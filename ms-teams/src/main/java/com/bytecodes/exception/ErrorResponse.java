012f843eab370aebd1bbc696c8330e79daf2f7b2
Andresd720 <andresd720@gmail.com>
2026-04-21 22:34:39 +0200

    Change type timestamps to string

--- ms-teams/src/main/java/com/bytecodes/exception/ErrorResponse.java
+++ ms-teams/src/main/java/com/bytecodes/exception/ErrorResponse.java
@@ -2,13 +2,8 @@ package com.bytecodes.exception;
 
 import java.time.Instant;
 
-public record ErrorResponse(String error,
-                            String message,
-                            Instant timestamp
-){public static ErrorResponse of(String error, String message) {
-    return new ErrorResponse(
-            error,
-            message,
-            Instant.now()
-    );
-}}
+public record ErrorResponse(String error, String message, String timestamp) {
+    public static ErrorResponse of(String error, String message) {
+        return new ErrorResponse(error, message, Instant.now().toString());
+    }
+}
