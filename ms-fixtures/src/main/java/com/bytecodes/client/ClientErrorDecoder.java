ba321432ac7ccb5ac5431b68a2c55ca02257662b
katarem <crismigesp@gmail.com>
2026-04-28 12:18:41 +0100

    fix: errors are as expected

--- ms-fixtures/src/main/java/com/bytecodes/client/ClientErrorDecoder.java
+++ ms-fixtures/src/main/java/com/bytecodes/client/ClientErrorDecoder.java
@@ -5,14 +5,11 @@ import feign.Response;
 import feign.codec.ErrorDecoder;
 import lombok.RequiredArgsConstructor;
 
-import java.util.Map;
-
 @RequiredArgsConstructor
 public class ClientErrorDecoder implements ErrorDecoder {
 
     @Override
     public Exception decode(String s, Response response) {
-        Map<String, String> errors = Map.of("EXTERNAL_API_CODE", response.status() + ": " + response.reason());
-        return new ExternalApiException(errors);
+        return new ExternalApiException(response.status() + ": " + response.reason());
     }
 }
--- ms-fixtures/src/main/java/com/bytecodes/exception/ErrorResponse.java
+++ ms-fixtures/src/main/java/com/bytecodes/exception/ErrorResponse.java
@@ -1,14 +1,11 @@
 package com.bytecodes.exception;
 
-import lombok.Builder;
-import lombok.Data;
-
 import java.time.Instant;
 
-@Data
-@Builder
-public class ErrorResponse<T> {
-    private String code;
-    private T errors;
-    private Instant timestamp;
+public record ErrorResponse(
+        String error,
+        String message,
+        Instant timestamp
+) {
+
 }
