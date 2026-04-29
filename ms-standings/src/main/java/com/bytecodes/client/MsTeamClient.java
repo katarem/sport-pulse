7df0a02e0290d61f394e3b99436723f1c7fcb521
Andresd720 <andresd720@gmail.com>
2026-04-29 23:19:38 +0200

    refacto and add annotations

--- ms-standings/src/main/java/com/bytecodes/client/MsTeamClient.java
+++ ms-standings/src/main/java/com/bytecodes/client/MsTeamClient.java
@@ -1,15 +1,20 @@
 package com.bytecodes.client;
 
+import com.bytecodes.dto.internal.TeamClientDTO;
 import com.bytecodes.dto.response.TeamDto;
+import jakarta.validation.Valid;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.cloud.openfeign.SpringQueryMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 @FeignClient(name="ms-teams",url = "${ms.teams.url}")
 public interface MsTeamClient {
 
-    @GetMapping("/api/teams/{teamId}")
-    TeamDto getTeamById(@PathVariable Integer teamId);
+    @GetMapping("/api/teams")
+    List<TeamClientDTO> getTeams(@Valid @SpringQueryMap StandingFilter filter);
 
 
 }
