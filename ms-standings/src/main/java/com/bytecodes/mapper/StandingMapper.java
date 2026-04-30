db7e97fc88fc3a53b979761541ea4d81aa6985f2
Andresd720 <andresd720@gmail.com>
2026-04-30 16:51:45 +0200

    create mapper for details

--- ms-standings/src/main/java/com/bytecodes/mapper/StandingMapper.java
+++ ms-standings/src/main/java/com/bytecodes/mapper/StandingMapper.java
@@ -5,6 +5,7 @@ import com.bytecodes.dto.external.StandingApiDTO;
 import com.bytecodes.dto.internal.TeamClientDTO;
 import com.bytecodes.dto.response.LeagueDTO;
 import com.bytecodes.dto.response.StandingDTO;
+import com.bytecodes.dto.response.StandingResponseDetailDTO;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -12,6 +13,7 @@ import org.mapstruct.Mapping;
 public interface StandingMapper {
 
 
+     //  Para el Get del /api/standings"
     @Mapping(target = "team.id",source = "clientDTO.id")
     @Mapping(target = "team.name",source = "clientDTO.name")
     @Mapping(target = "team.logo",source = "clientDTO.logo")
@@ -27,6 +29,29 @@ public interface StandingMapper {
     @Mapping(target = "goalDifference",source = "standingApiDTO.goalsDiff")
     @Mapping(target = "form",source = "standingApiDTO.form")
     StandingDTO standingApiAndTeamClientToResponseDto(StandingApiDTO standingApiDTO, TeamClientDTO clientDTO);
+
+
+    // responde al endpoint GET/api/standing/team/{teamid}
+
+    @Mapping(target = "team.id",source = "clientDTO.id")
+    @Mapping(target = "team.name",source = "clientDTO.name")
+    @Mapping(target = "team.logo", ignore = true)
+
+    @Mapping(target = "league.id",source = "leagueApi.id")
+    @Mapping(target = "league.name",source = "leagueApi.name")
+    @Mapping(target = "league.country", ignore = true)
+    @Mapping(target = "league.season", ignore = true)
+
+    @Mapping(target = "season",source = "leagueApi.season")
+    @Mapping(target = "rank",source = "standingApi.rank")
+    @Mapping(target = "played",source = "standingApi.all.played")
+    @Mapping(target = "points",source = "standingApi.points")
+    @Mapping(target = "form",source = "standingApi.form")
+    @Mapping(target = "description",source = "standingApi.description")
+    StandingResponseDetailDTO standingApiAndTeamClientAndLeagueToDeatailResponseDto(StandingApiDTO standingApi,
+                                                                                    TeamClientDTO clientDTO,
+                                                                                    LeagueApiDTO leagueApi);
+
 
     LeagueDTO toLeagueDTO(LeagueApiDTO league);
 
